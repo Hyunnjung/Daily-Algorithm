b527dc2b817347676ef7a653cc45a7f48af3302d
hyunjungee <hyubjung96@gmail.com>
2021-02-01 17:27:03 +0900

    solve BOJ_2110.py

--- Baekjoon_Online_Judge/2021.01/BOJ_2110.py
+++ Baekjoon_Online_Judge/2021.01/BOJ_2110.py
@@ -7,15 +7,15 @@ N, C = map(int, input().split())
 house = [int(sys.stdin.readline()) for _ in range(N)]
 house.sort()
 
-start = 1 # 최소 간격
+start = 1# 최소 간격
 end = house[-1] - house[0] #최대 간격
 answer = 0
 
 while start <= end:
-    mid = (start + end) // 2
+    mid = (start + end) // 2 # mid는 가장 인접한 두 공유기 사이의 거리 의미
     count = 1 # 공유기의 개수(처음 설치하고 시작)
     cur = house[0]
-    for i in range(1, N):
+    for i in range(1, N): # 현재의mid값 이용해 앞에서부터 설이
         if house[i] - cur >= mid:
             count += 1
             cur = house[i]
@@ -28,5 +28,6 @@ while start <= end:
         end = mid - 1
 
 print(answer)
+
 
 
